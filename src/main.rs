0d1b74fddd6f44512eff93f02a6986fd9d7bdb20
Serg <1899416+dontpullthis@users.noreply.github.com>
2022-01-20 23:23:36 +0200

    A function to refresh definitions

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,24 @@
 mod config;
 mod tasks;
 
-use std::fs;
 use std::io::Error;
 
 use env_logger;
 use log;
-use serde_yaml;
 
 use crate::config::Config;
-use crate::tasks::manager::Manager;
+use crate::tasks::manager::{Manager, refresh_definitions};
 
 fn main() -> Result<(), Error> {
     env_logger::builder().parse_env("LOG_LEVEL").init();
-    log::info!("Starting...");
+    log::info!("Initialization...");
     let config = Config::new()?;
     let mut task_manager = Manager::new();
-    for entry in fs::read_dir(config.dir_tasks)? {
-        let entry = entry?;
-        let path = entry.path().display().to_string();
-        if entry.path().is_dir() || !path.ends_with(".yaml") {
-            continue;
-        }
+    log::info!("Starting...");
 
-        log::debug!("Scanning file {}...", &path);
-        let contents = fs::read_to_string(&path)?;
-        let tasks = match serde_yaml::from_str(&contents) {
-            Ok(t) => t,
-            Err(e) => {
-                log::error!("Failed to parse the YAML file \"{}\": {}", &path, e);
-                continue
-            }  
-        };
+    // Definitions might be refreshed with some interval in thread
+    refresh_definitions(&config, &mut task_manager)?;
 
-        task_manager.merge_definitions(&tasks);
-    }
     log::info!("Application terminated.");
     Ok(())
 }
--- src/tasks/manager.rs
+++ src/tasks/manager.rs
@@ -1,5 +1,12 @@
-use std::collections::HashMap;
 
+use std::collections::HashMap;
+use std::fs;
+
+use std::io::Error;
+
+use serde_yaml;
+
+use crate::config::Config;
 use crate::tasks::task_definition::TaskDefinition;
 
 pub struct Manager {
@@ -17,6 +24,29 @@ impl Manager {
         for def in new_defs {
             self.definitions.insert(def.id.clone(), def.clone());
         }
-        // self.definitions.append(&mut new_defs); // TODO: lookup tasks by ID, merge exisiting tasks
     }
+}
+
+pub fn refresh_definitions(config: &Config, manager: &mut Manager) -> Result<(), Error> {
+    for entry in fs::read_dir(&config.dir_tasks)? {
+        let entry = entry?;
+        let path = entry.path().display().to_string();
+        if entry.path().is_dir() || !path.ends_with(".yaml") {
+            continue;
+        }
+
+        log::debug!("Scanning file {}...", &path);
+        let contents = fs::read_to_string(&path)?;
+        let tasks = match serde_yaml::from_str(&contents) {
+            Ok(t) => t,
+            Err(e) => {
+                log::error!("Failed to parse the YAML file \"{}\": {}", &path, e);
+                continue
+            }
+        };
+
+        manager.merge_definitions(&tasks);
+    }
+
+    Ok(())
 }
